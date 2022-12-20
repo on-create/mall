44fa130224cd3d464a95a7fe7f928ae9400d23bd
on-create <zhong.ming.shan@outlook.com>
2022-12-20 17:26:18 +0800

    修改pom文件，使用nacos注册服务，openfeign测试服务是否可用

---
+++ mall-member/src/main/java/com/example/mall/member/feign/CouponFeignService.java
@@ -0,0 +1,12 @@
+package com.example.mall.member.feign;
+
+import com.example.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("mall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    R memberCoupons();
+}
